89125e6b1cc294d51affa5fff679c4b87d2d4c57
maguro <maguro.devlife@gmail.com>
2021-05-10 22:03:41 +0900

    authのロジックをuseAuthへ移行

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,11 +1,37 @@
-import { ref } from '@nuxtjs/composition-api'
-import { AuthState } from '@aws-amplify/ui-components' 
+import { defineComponent, onMounted, provide, inject, useAsync, ref, Ref, useFetch } from '@nuxtjs/composition-api'
+import useToDoList from '@/composables/useToDoList'
+import ToDoListKey from '~/composables/keys/useToDoListKey'
+import { onAuthUIStateChange, AuthState} from '@aws-amplify/ui-components'
+import { Auth } from '@aws-amplify/auth'
+import { CognitoUser } from 'amazon-cognito-identity-js'
+import AuthKey from '@/composables/keys/useAuthKey'
+import useUser from '@/composables/useUser'
+import UserKey from '@/composables/keys/useUserKey'
 
 const useAuth = () => {
   const authState = ref<AuthState | undefined>(undefined)
 
+  const { user } = inject(UserKey, useUser())
+  const { toDos, getToDoList, subscribe } = inject(ToDoListKey, useToDoList())
+
+  onAuthUIStateChange((state) => {
+    authState.value = state
+    console.log(state)
+
+    if (state === AuthState.SignedOut) toDos.list = []
+    if (state === AuthState.SignedIn) {
+      Auth.currentAuthenticatedUser()
+        .then(result => {
+          user.value = result
+          if (user.value === undefined || user.value === null) return
+          getToDoList(user.value.getUsername())
+          subscribe()
+          })
+    }
+  })
+
   return {
-    authState
+    authState,
   }
 }
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -12,52 +12,17 @@
 
 <script lang='ts'>
 import { defineComponent, onMounted, provide, inject, useAsync, ref, Ref, useFetch } from '@nuxtjs/composition-api'
-import useToDoList from '@/composables/useToDoList'
-import ToDoListKey from '~/composables/keys/useToDoListKey'
-import { onAuthUIStateChange, AuthState} from '@aws-amplify/ui-components'
-import { Auth } from '@aws-amplify/auth'
-import { CognitoUser } from 'amazon-cognito-identity-js'
 import useAuth from '@/composables/useAuth'
-import AuthKey from '@/composables/keys/useAuthKey'
 import useUser from '@/composables/useUser'
 import UserKey from '@/composables/keys/useUserKey'
+import useToDoList from '@/composables/useToDoList'
+import ToDoListKey from '~/composables/keys/useToDoListKey'
 
 export default defineComponent({
   setup() {
-    provide(ToDoListKey, useToDoList())
-    provide(AuthKey, useAuth())
     provide(UserKey, useUser())
-    const { toDos, getToDoList, subscribe } = inject(ToDoListKey, useToDoList())
-    const { authState } = inject(AuthKey, useAuth())
-    const { user } = inject(UserKey, useUser())
-
-    onAuthUIStateChange((state) => {
-      authState.value = state
-
-      if (state === AuthState.SignedOut) toDos.list = []
-      if (state === AuthState.SignedIn) {
-        Auth.currentAuthenticatedUser()
-          .then(result => {
-            user.value = result
-            if (user.value === undefined || user.value === null) return
-            getToDoList(user.value.getUsername())
-            subscribe()
-            })
-      }
-    })
-
-    // onAuthUIStateChange(async (authState) => {
-    //   if (authState === AuthState.SignOut) toDos.list = []
-    //   const user = await Auth.currentUserInfo()
-    //   if (user === null) return
-    //   await getToDoList(user.username)
-    //   console.log(toDos)
-    //   subscribe()
-    // })
-
-    return {
-      toDos
-    }
+    provide(ToDoListKey, useToDoList())
+    useAuth()
   }
 })
 
